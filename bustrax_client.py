bf2a1dfa828f20b0d54864f585ee5d3529ae282a
globalvoice <154972750+globalvoice@users.noreply.github.com>
2025-05-28 01:59:09 +0300

    Update bustrax_client.py

--- bustrax_client.py
+++ bustrax_client.py
@@ -1,10 +1,38 @@
+import os
 import requests
 
-def get_driver_alerts(api_key):
-    # This function should call the first/second API and return parsed alerts
-    # Replace this with actual API request logic per your API doc
-    url = "https://api.bustrax.com/alerts"
-    headers = {"Authorization": f"Bearer {api_key}"}
-    resp = requests.get(url, headers=headers)
+LOGIN_URL = os.getenv("BUSTRAX_LOGIN_URL")
+API_URL = os.getenv("BUSTRAX_API_URL")
+USERNAME = os.getenv("BUSTRAX_USERNAME")
+PASSWORD = os.getenv("BUSTRAX_PASSWORD")
+VER = os.getenv("BUSTRAX_VER", "1.0.1")
+BUNIT = os.getenv("BUSTRAX_BUNIT")
+ANTICIPATION_MINUTES = os.getenv("BUSTRAX_ANTICIPATION_MINUTES", "45")
+
+def get_bustrax_token():
+    resp = requests.post(
+        LOGIN_URL,
+        data={
+            "username": USERNAME,
+            "password": PASSWORD,
+            "version": "2.0"
+        }
+    )
     resp.raise_for_status()
-    return resp.json()["alerts"]
+    data = resp.json()
+    # Get token from 3rd position as per doc
+    token = data["data"][2]["api_key"]
+    return token
+
+def get_route_tracking(token):
+    form = {
+        "data[iuser]": USERNAME,
+        "data[bttkn]": token,
+        "data[ver]": VER,
+        "data[bunit]": BUNIT,
+        "data[anticipation_minutes]": ANTICIPATION_MINUTES,
+        "type": "get_route_tracking"
+    }
+    resp = requests.post(API_URL, data=form)
+    resp.raise_for_status()
+    return resp.json()
