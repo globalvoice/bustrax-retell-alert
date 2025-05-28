22dc76cd4b782f27688e15057fd277434925d4cb
globalvoice <154972750+globalvoice@users.noreply.github.com>
2025-05-28 04:59:04 +0300

    Delete utils.py

--- utils.py
+++
@@ -1,9 +0,0 @@
-def format_number(phone):
-    """
-    Formats the phone number by removing spaces/dashes and ensuring +52 country code.
-    Leading zeros are removed.
-    """
-    phone = str(phone).replace(" ", "").replace("-", "")
-    if not phone.startswith("+52"):
-        phone = "+52" + phone.lstrip("0")
-    return phone
